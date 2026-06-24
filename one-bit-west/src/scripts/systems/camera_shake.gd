d880e666d61a017603ba44c4648ddcd4735f7a5f
RizelBeri <yevrostik@gmail.com>
2026-06-25 01:40:01 +0300

    bebbebebebebe
    - fix player camera shake
    - main scene setup

--- one-bit-west/src/scripts/systems/camera_shake.gd
+++ one-bit-west/src/scripts/systems/camera_shake.gd
@@ -5,10 +5,11 @@ var shake_duration: float = 0.0
 var walk_intensity: float = 0.02 # Footstep bob strength
 var walk_speed: float = 12.0 # Bob frequency
 var noise_time: float = 0.0
-
+var base_position: Vector3 = Vector3.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	base_position = position
 	print("parent: ", get_parent())
 	print("grandparent: ", get_parent().get_parent())
 	print("is CharacterBody3D: ", get_parent().get_parent() is CharacterBody3D)
@@ -30,4 +31,4 @@ func _process(delta: float) -> void:
 			bob_offset.y = sin(noise_time * walk_speed) * walk_intensity * speed_factor
 			bob_offset.x = cos(noise_time * walk_speed * 0.5) * walk_intensity * 0.5 * speed_factor
 
-	rotation = bob_offset
+	position = base_position + bob_offset
