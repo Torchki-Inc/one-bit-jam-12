e445539e82f1bcf71f3eb79965d59fa7e9054d25
RizelBeri <yevrostik@gmail.com>
2026-06-24 20:33:00 +0300

    first working enemy

--- one-bit-west/src/scripts/enemies/revolver_bandit.gd
+++ one-bit-west/src/scripts/enemies/revolver_bandit.gd
@@ -2,7 +2,8 @@ class_name BanditRevolver
 extends BaseEnemy
 
 var sm: EnemySM
-@export var revolver_damage := 2
+@onready var shoot_point: Marker3D = $ShootPoint
+
 
 
 func _ready():
@@ -31,8 +32,33 @@ func _physics_process(delta: float) -> void:
 	sm.update(delta)
 
 
-func shoot():
-	pass
+func make_shot():
+	var space_state := get_world_3d().direct_space_state
+
+	var from := self.shoot_point.global_position
+	var direction := (player.global_position - from).normalized()
+	var to: Vector3 = from + direction * self.shoot_radius
+
+	var query := PhysicsRayQueryParameters3D.create(from, to)
+	query.exclude = [self]
+
+
+	var result := space_state.intersect_ray(query)
+
+	if result:
+		var hit_object = result["collider"]
+
+		if hit_object.has_method("take_damage"):
+			hit_object.take_damage(self.damage)
+
+			print("Revolver hit: ", hit_object.name)
+
+		elif hit_object.get_parent().has_method("take_damage"):
+			hit_object.get_parent().take_damage(self.damage)
+
+			print("Revolver hit: ", hit_object.name)
+	else:
+		print(self.get_instance_id(), " misses")
 
 #region States
 
@@ -74,7 +100,7 @@ class BanditPrepareState extends EnemyState:
 
 	func enter():
 		timer = 0.8
-		# enemy.anim_state.travel("prepare") 
+		# enemy.anim_state.travel("prepare")
 
 
 	func update(delta) -> EnemyState:
@@ -90,7 +116,7 @@ class BanditShootState extends EnemyState:
 
 	func enter():
 		print("Enter Soot state")
-		enemy.shoot()
+		enemy.make_shot()
 
 		pass
 		# enemy.anim_state.travel("shoot")
