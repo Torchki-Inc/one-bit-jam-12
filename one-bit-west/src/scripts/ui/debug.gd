5851f9c99f137903c3f133d654b4418336f9be43
RizelBeri <yevrostik@gmail.com>
2026-06-27 00:40:42 +0300

    debug info

---
+++ one-bit-west/src/scripts/ui/debug.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var fps_label = $VBoxContainer/Fps
+@onready var enemies_alive_label = $VBoxContainer/EnemiesAlive
+@onready var wave_time_label = $VBoxContainer/WaveTimeLeft
+
+var game_manager: Node # assign in editor
+
+
+func _process(_delta: float) -> void:
+	if not game_manager:
+		game_manager = get_node_or_null("../../Systems/GameManager")
+		return
+
+	fps_label.text = "FPS: %d" % Engine.get_frames_per_second()
+	enemies_alive_label.text = "Enemies: %d" % game_manager.get_alive_enemy_count()
+	wave_time_label.text = "Time: %.1f" % game_manager.timer.time_left # adjust to actual property
