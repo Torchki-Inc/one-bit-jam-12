7c728e3fefedd7cf6ec040a7ee145efe9c252092
RizelBeri <yevrostik@gmail.com>
2026-06-27 01:45:43 +0300

    revork enemy stats

--- one-bit-west/src/scripts/ui/main_menu.gd
+++ one-bit-west/src/scripts/ui/main_menu.gd
@@ -4,6 +4,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	pass # Replace with function body.
 
 
--- one-bit-west/src/scripts/weapons/weapon.gd
+++ one-bit-west/src/scripts/weapons/weapon.gd
@@ -8,6 +8,7 @@ enum WeaponType {
 @export var type := WeaponType.REVOLVER
 
 # Revolver stats
+@export_group("Revolver")
 @export var revolver_damage := 2
 @export var revolver_range := 30.0
 @export var revolver_fire_rate := 0.35
@@ -17,6 +18,7 @@ enum WeaponType {
 @export var revolver_reload_time := 1.2
 
 # Shotgun stats
+@export_group("Shotgun")
 @export var shotgun_damage := 1
 @export var shotgun_range := 15.0
 @export var shotgun_fire_rate := 0.9
